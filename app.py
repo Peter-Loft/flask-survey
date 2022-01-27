c738b1fc39e58028ad53d8116a5b587f4bddc740
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-27 18:44:44 -0500

    Added session

--- app.py
+++ app.py
@@ -8,33 +8,37 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+
 
 @app.get("/")
 def homepage():
-
+    """Generates landing page"""
+    session["responses"] = []
     return render_template("survey_start.html", survey=survey)
 
 @app.get("/question/<int:questionnum>")
 def questionpage(questionnum):
-
+    """Route for questions after first one"""
     question = survey.questions[questionnum]
 
 
-    return render_template("question.html", question=question, choices = question.choices)
+    return render_template("question.html", question=question)
 
 @app.post("/begin")
 def first_question():
-
+    """Route for first question"""
+    
     question = survey.questions[0]
 
-    return render_template("question.html", questionnum = 0, question=question, choices = question.choices)
+    return render_template("question.html", question=question)
 
 @app.post("/answer")
 def answer():
-
+    """Saves answer user submitted and redirect to next question or 
+    completed page"""
+    responses = session["responses"]
     responses.append(request.form.get("answer"))
-
+    session["responses"] = responses
     if len(responses) < len(survey.questions):
         return redirect(f"/question/{len(responses)}")
     else:
@@ -42,4 +46,5 @@ def answer():
 
 @app.get("/completion")
 def completion():
+    """Generates completion page"""
     return render_template("completion.html")
