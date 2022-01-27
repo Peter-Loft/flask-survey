2a5fea964a650f5725a5b6da778f08e70a7ed057
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-27 14:26:35 -0600

    Completed first steps to get through entire survey

--- app.py
+++ app.py
@@ -15,10 +15,31 @@ def homepage():
 
     return render_template("survey_start.html", survey=survey)
 
-@app.get("/questions/<int:questionnum>")
+@app.get("/question/<int:questionnum>")
 def questionpage(questionnum):
 
     question = survey.questions[questionnum]
 
 
-    return render_template("question.html", questionnum = 0)
+    return render_template("question.html", question=question, choices = question.choices)
+
+@app.post("/begin")
+def first_question():
+
+    question = survey.questions[0]
+
+    return render_template("question.html", questionnum = 0, question=question, choices = question.choices)
+
+@app.post("/answer")
+def answer():
+
+    responses.append(request.form.get("answer"))
+
+    if len(responses) < len(survey.questions):
+        return redirect(f"/question/{len(responses)}")
+    else:
+        return redirect("/completion")
+
+@app.get("/completion")
+def completion():
+    return render_template("completion.html")
